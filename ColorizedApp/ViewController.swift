4e1e6f08409e9e41dadeaed34b11fa8d274985d6
breetva <breetva@gmail.com>
2022-07-14 15:28:15 +0200

    Some code and view optimization

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -11,51 +11,61 @@ class ViewController: UIViewController {
 
     @IBOutlet var colorizedView: UIView!
     
-    @IBOutlet var redValueLabel: UILabel!
-    @IBOutlet var greenValueLabel: UILabel!
-    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
     
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         colorizedView.layer.cornerRadius = 15
-        colorizedView.backgroundColor = .black
-        setupSlider()
-        setupLabel()
+        setColor()
+        setLabelValue(for: redLabel, greenLabel, blueLabel)
     }
 
-    @IBAction func sliderAction() {
-        redValueLabel.text = formattingValueLabel(toString: redSlider.value)
-        greenValueLabel.text = formattingValueLabel(toString: greenSlider.value)
-        blueValueLabel.text = formattingValueLabel(toString: blueSlider.value)
-        colorizedView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redLabel.text = roundedStringValue(from: redSlider)
+        case greenLabel:
+            greenLabel.text = roundedStringValue(from: greenSlider)
+        default:
+            blueLabel.text = roundedStringValue(from: blueSlider)
+        }
     }
     
     // Private Methods
-    private func formattingValueLabel(toString value: Float) -> String {
-        return String(format: "%.2f", value)
+    private func setColor() {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
     }
     
-    private func setupLabel() {
-        redValueLabel.text = formattingValueLabel(toString: redSlider.value)
-        greenValueLabel.text = formattingValueLabel(toString: greenSlider.value)
-        blueValueLabel.text = formattingValueLabel(toString: blueSlider.value)
+    private func roundedStringValue(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
     
-    private func setupSlider() {
-        redSlider.value = 0
-        redSlider.minimumTrackTintColor = .red
-        
-        greenSlider.value = 0
-        greenSlider.minimumTrackTintColor = .green
-        
-        blueSlider.value = 0
-        blueSlider.minimumTrackTintColor = .blue
-        
+    private func setLabelValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                redLabel.text = roundedStringValue(from: redSlider)
+            case greenLabel:
+                greenLabel.text = roundedStringValue(from: greenSlider)
+            default:
+                blueLabel.text = roundedStringValue(from: blueSlider)
+            
+            }
+        }
     }
 
 }
